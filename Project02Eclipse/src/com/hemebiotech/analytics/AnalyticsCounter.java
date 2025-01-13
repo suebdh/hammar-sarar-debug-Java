7ed6e215f943998a961fec3460bab440b8072966
Sarar Hammar <hammar.sarar@gmail.com>
2025-01-13 18:12:47 +0100

    remove unnecessary comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,64 @@
 package com.hemebiotech.analytics;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
+/**
+ * This class will read a file, count occurrences of three symptoms ("headache", "rash", "pupils"),
+ *  and write the results in an output file.
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+    private static Logger logger = LogManager.getLogger(AnalyticsCounter.class);
+    // Initialize counters for each symptom
+    static int headacheCount = 0;	// counts headache occurencies
+    static int  rashCount =0; // counts rash occurencies
+    static int  pupilCount = 0; // counts dialated pupils occurencies
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+    public static void main(String args[]) throws Exception {
+
+        // Specify the input and output files
+        String inputFile = "symptoms.txt";
+        String outputFile = "result.out";
+
+
+        //Step 1 : Read symtoms from input file
+        try {
+            ISymptomReader reader = new ReadSymptomDataFromFile(inputFile);
+            List<String> symptoms = reader.getSymptoms();
+
+            // Step 2: Count occurrences of each symptom
+            for (String symptom : symptoms) {
+                if (symptom.equals("headache")) {
+                    headacheCount++;
+                } else if (symptom.equals("rash")) {
+                    rashCount++;
+                } else if (symptom.contains("dialated pupils")) {
+                    pupilCount++;
+
+                }
+            }
+        } catch (IOException e) {
+            logger.error("Erreur de Input File");
+        }
+        //TO DO Step 3 : Order by alphabatically
+
+        // TO DO Step 4 : write result to the output file
+       try{
+           FileWriter writer = new FileWriter(outputFile);
+           writer.write("headache: " + headacheCount + "\n");
+           writer.write("rash: " + rashCount + "\n");
+           writer.write("dialated pupils: " + pupilCount);
+           writer.close();
+       }catch(IOException e) {
+           logger.error("Erreur de Output File");
+       }
+
+    }
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,6 +1,10 @@
 package com.hemebiotech.analytics;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,40 +12,49 @@ import java.util.List;
 
 /**
  * Simple brute force implementation
- *
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	private String filepath;
-	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
-	}
-	
-	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
-				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
-				}
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
-	}
+    private static Logger logger = LogManager.getLogger(ReadSymptomDataFromFile.class);
+    private String filepath;
+
+    /**
+     * @param filepath a full or partial path to file with symptom strings in it, one per line
+     */
+    public ReadSymptomDataFromFile(String filepath) {
+
+        this.filepath = filepath;
+    }
+
+    /**
+     * This method reads the text file in entry line by line
+     *
+     * @return result : ArrayList containing all symtoms (possible duplicates)
+     * @throws FileNotFoundException if the opening or reading of a file was failed ; IOException if erros of Input/output
+     */
+    @Override
+    public List<String> getSymptoms() throws IOException {
+        ArrayList<String> result = new ArrayList<String>();
+
+        if (filepath != null) {
+            try {
+                logger.info("Fichier d'entrée en lecture");
+                BufferedReader reader = new BufferedReader(new FileReader(filepath));
+                String line = reader.readLine();
+
+                while (line != null) {
+                    result.add(line);
+                    line = reader.readLine();
+                }
+                reader.close();
+            } catch (FileNotFoundException e) {
+                logger.error("Fichier non trouvé");
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+            }
+        }
+
+        return result;
+    }
 
 }
